a07fe3c8885dd934cde9389b4d884269f75175cb
shovon005 <144725632+shovon005@users.noreply.github.com>
2023-10-04 21:21:37 +0600

    Add files via upload

---
+++ window.js
@@ -0,0 +1,7 @@
+//window open and close event
+var a = "are you sure"
+console.log(window);
+console.log(window.alert("wlecome to our site"));
+console.log(window.open("https://www.google.com/"));
+console.log(window.confirm('Would you like to play game?'));
+
