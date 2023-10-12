770ed8ae0f4d6bd22d3727582da0c6de6b443ef9
shovon005 <144725632+shovon005@users.noreply.github.com>
2023-10-12 13:48:52 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+//array mathod
+let num = [1,2,3,4,5]
+let b = num.toString()//b is now string
+console.log(b);
+ let c= num.join("_")//join mathod
+console.log(c);
+//pop for out number
+num.pop()
+console.log(num);
+//for add number 
+num.push(10)
+console.log(num);
+//shift
+let r = num.shift()
+console.log(r,num);  
+
+
+
