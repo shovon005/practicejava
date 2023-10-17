6fa772bb6c557cef82429af7f4c994ec6b6eda6f
shovon005 <144725632+shovon005@users.noreply.github.com>
2023-10-17 18:56:11 +0600

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,17 @@
+//for
+for (let num = 1; num <= 10; num++) {
+    console.log(num++);
+}//
+//while
+while(i<=5){
+    const string = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    let i = 0;
+    let alphabet = "";
+    
+    while (i < 5) {
+      alphabet += string[i];
+      i++;
+    }
+    console.log(alphabet);
+    
+}
